b413553a54219316f9c48490c3f67b53c0fe18fa
Ben Baiju <ben.baiju2001@gmail.com>
2026-08-03 18:54:17 +1000

    Added Logging

--- applications/receiver_app.py
+++ applications/receiver_app.py
@@ -1,13 +1,25 @@
-from ramp_udp.transport.receiver import Receiver
+from ramp_udp.transport.udp_receiver import UDPReceiver
 
 
 def main():
-    receiver = Receiver("127.0.0.1", 5001)
+    receiver = UDPReceiver("127.0.0.1", 5001)
+
+    print("\n RAMP-UDP Receiver")
+    print("\nListening on 127.0.0.1:5001...\n")
 
     packet, address = receiver.receive()
 
-    print(f"Received from {address}")
-    print(packet)
+    print(f"Packet received from {address}")
+
+    print("\nDeserializing packet...")
+    print(" Packet deserialized")
+
+    print(f"Magic           : {packet.magic.decode()}")
+    print(f"Version         : {packet.version}")
+    print(f"Message Type    : {packet.message_type.name}")
+    print(f"Sequence Number : {packet.sequence_number}")
+    print(f"Payload Length  : {packet.payload_length} bytes")
+    print(f"Payload         : {packet.payload.decode()}")
 
     receiver.close()
 
--- applications/sender_app.py
+++ applications/sender_app.py
@@ -1,10 +1,11 @@
 from ramp_udp.protocol.message_types import MessageType
 from ramp_udp.protocol.packet import Packet
-from ramp_udp.transport.sender import Sender
+from ramp_udp.protocol.serializer import PacketSerializer
+from ramp_udp.transport.udp_sender import UDPSender
 
 
 def main():
-    sender = Sender("127.0.0.1", 5000)
+    sender = UDPSender("127.0.0.1", 5000)
 
     packet = Packet(
         message_type=MessageType.DATA,
@@ -12,7 +13,25 @@ def main():
         payload=b"Hello RAMP-UDP",
     )
 
+    print("\n RAMP-UDP Sender Application")
+
+    print("\nCreating packet...")
+    print(" Packet created")
+
+    print(f"Message Type    : {packet.message_type.name}")
+    print(f"Sequence Number : {packet.sequence_number}")
+    print(f"Payload Length  : {packet.payload_length} bytes")
+    print(f"Payload         : {packet.payload.decode()}")
+
+    print("\nSerializing packet...")
+    data = PacketSerializer.serialize(packet)
+    print(" Packet serialized")
+    print(f"Packet Size     : {len(data)} bytes")
+
+    print("\nSending packet...")
     sender.send(packet, ("127.0.0.1", 5001))
+    print(" Packet sent")
+
     sender.close()
 
 
